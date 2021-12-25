b88ccd639ad8e5fdde8e4099b1ed1dbfa73f878b
Broderick Westrope <asclepiius.unknown@gmail.com>
2021-12-26 08:57:03 +1100

    Fix removeLine for Windows OS

--- src/blackjack/utilities.py
+++ src/blackjack/utilities.py
@@ -7,13 +7,18 @@ def clearCLI():
         os.system('CLS')
     elif os.name == 'posix':
         os.system('clear')
-        pass
     else:
         print("** ERROR: (main.py) Unable to clear screen. **")
 
 
 def removeLine():
-    tput = subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE)
-    cols = int(tput.communicate()[0].strip())
-    print("\033[A{}\033[A".format(' '*cols) + "\n")
+    if os.name == 'nt':
+        print("\033[A                                                                                                                                                                                              \033[A\n")
+    elif os.name == 'posix':
+        tput = subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE)
+        cols = int(tput.communicate()[0].strip())
+        print("\033[A{}\033[A".format(' '*cols) + "\n")
+    else:
+        print("** ERROR: (main.py) Unable to clear screen. **")
+    
     # print()
